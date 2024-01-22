7c92986eb0d3d989ff70965620325fb62cd96e28
Paul Jeremiah Mugaya <paulmugaya@live.com>
2024-01-22 16:53:59 +0300

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "preline.co",
+        port: "",
+        pathname: "/assets/img/mockups/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
